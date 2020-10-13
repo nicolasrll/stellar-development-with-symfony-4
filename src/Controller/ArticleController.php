4e2d0c62d1ed032bcffcd23c9916813d8e0cbb3e
Nicolas Rellier <nicolasrellier@yahoo.fr>
2020-10-13 21:30:02 +0200

    generating urls

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,15 +9,15 @@ use Symfony\Component\HttpFoundation\Response;
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response("First object Response returned !");
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
